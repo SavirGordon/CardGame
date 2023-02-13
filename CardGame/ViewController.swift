2b873fa243b17138c9581b88e34f83bca6ecb2d6
SavirGordon <savirgordon83@gmail.com>
2023-02-13 12:18:24 -0500

    k

--- CardGame/ViewController.swift
+++ CardGame/ViewController.swift
@@ -8,12 +8,59 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var score = 0
+    var currentCardValue = 0
+    var nextCardValue = 0
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+         currentCardValue = Int.random(in: 1 ... 13)
+
+        Score.text = "Score: \(score)"
+        image.image = UIImage(named:"\(currentCardValue)")
     }
 
-
+    @IBOutlet var image: UIImageView!
+    @IBOutlet var Score: UILabel!
+    @IBOutlet var higher: UIButton!
+    @IBOutlet var lower: UIButton!
+    
+    @IBAction func lowerButton(_ sender: Any) {
+        nextCardValue = randomNum()
+        image.image = UIImage(named:"\(nextCardValue)")
+        if nextCardValue < currentCardValue {
+            score += 1
+            currentCardValue = nextCardValue
+            Score.text = "Score: \(score)"
+        }
+        else {
+            Score.text = "Game Over"
+            score = 0
+        }
+    }
+    
+    @IBAction func higherButton(_ sender: Any) {
+        
+        nextCardValue = randomNum()
+        image.image = UIImage(named:"\(nextCardValue)")
+        if nextCardValue > currentCardValue {
+            score += 1
+            currentCardValue = nextCardValue
+            Score.text = "Score: \(score)"
+        }
+        else {
+            Score.text = "Game Over"
+            score = 0
+            
+        }
+    }
+    
+    func randomNum () -> Int{
+        return Int.random(in: 1 ... 13)
+    }
+    
 }
 
